cc8915a8f8cc4ee74160c0f7cbbdf3457dbda670
UpHash <yukawallstudio@gmail.com>
2021-05-16 02:54:29 +0900

    型付きでHook

--- Vtil/Vtil.cpp
+++ Vtil/Vtil.cpp
@@ -63,40 +63,74 @@ extern "C"
 	FARPROC PA;
 	int JMPtoAPI();
 }
-void WINAPI Vtil::Native::AIAudioAPI_DeviceInfo() { PA = p[0]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_ModuleFlag() { PA = p[1]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_LicenseInfo() { PA = p[2]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_LicenseDate() { PA = p[3]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AIAudioAPI_Open() { PA = p[4]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AIAudioAPI_Close() { PA = p[5]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AIAudioAPI_PushData() { PA = p[6]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AIAudioAPI_PushEvent() { PA = p[7]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AIAudioAPI_ClearData() { PA = p[8]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AIAudioAPI_Suspend() { PA = p[9]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AIAudioAPI_Resume() { PA = p[10]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AIAudioAPI_SaveWave() { PA = p[11]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AIAudioAPI_GetDescriptor() { PA = p[12]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_Init() { PA = p[13]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_End() { PA = p[14]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_VersionInfo() { PA = p[15]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_GetStatus() { PA = p[16]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_SetParam() { PA = p[17]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_GetParam() { PA = p[18]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_LangLoad() { PA = p[19]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_LangClear() { PA = p[20]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_VoiceLoad() { PA = p[21]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_VoiceClear() { PA = p[22]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_TextToSpeech() { PA = p[23]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_CloseSpeech() { PA = p[24]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_GetData() { PA = p[25]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_TextToKana() { PA = p[26]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_CloseKana() { PA = p[27]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_GetKana() { PA = p[28]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_GetJeitaControl() { PA = p[29]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_BLoadWordDic() { PA = p[30]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_ReloadWordDic() { PA = p[31]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_ReloadPhraseDic() { PA = p[32]; JMPtoAPI(); }
-void WINAPI Vtil::Native::AITalkAPI_ReloadSymbolDic() { PA = p[33]; JMPtoAPI(); }
+Vtil::Native::AIAudioResultCode WINAPI Vtil::Native::AIAudioAPI_DeviceInfo(LPTSTR guid, LPTSTR name, INT32 bufferLen, INT32& requireLen)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AIAudioAPI_DeviceInfo>(p[0])(guid, name, bufferLen, requireLen); }
+UINT32 WINAPI Vtil::Native::AITalkAPI_ModuleFlag()
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_ModuleFlag>(p[1])(); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_LicenseInfo(LPTSTR key, LPTSTR str, INT32 len)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_LicenseInfo>(p[2])(key, str, len); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_LicenseDate(LPTSTR data)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_LicenseDate>(p[3])(data); }
+Vtil::Native::AIAudioResultCode WINAPI Vtil::Native::AIAudioAPI_Open(Vtil::Native::AIAudio_TConfig& config)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AIAudioAPI_Open>(p[4])(config); }
+Vtil::Native::AIAudioResultCode WINAPI Vtil::Native::AIAudioAPI_Close()
+{ return reinterpret_cast<Vtil::Native::Callbacks::AIAudioAPI_Close>(p[5])(); }
+Vtil::Native::AIAudioResultCode WINAPI Vtil::Native::AIAudioAPI_PushData(UINT8* buf, UINT32 len, INT32 stop)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AIAudioAPI_PushData>(p[6])(buf, len, stop); }
+Vtil::Native::AIAudioResultCode WINAPI Vtil::Native::AIAudioAPI_PushEvent(UINT64 tick, void* userData)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AIAudioAPI_PushEvent>(p[7])(tick, userData); }
+Vtil::Native::AIAudioResultCode WINAPI Vtil::Native::AIAudioAPI_ClearData()
+{ return reinterpret_cast<Vtil::Native::Callbacks::AIAudioAPI_ClearData>(p[8])(); }
+Vtil::Native::AIAudioResultCode WINAPI Vtil::Native::AIAudioAPI_Suspend()
+{ return reinterpret_cast<Vtil::Native::Callbacks::AIAudioAPI_Suspend>(p[9])(); }
+Vtil::Native::AIAudioResultCode WINAPI Vtil::Native::AIAudioAPI_Resume()
+{ return reinterpret_cast<Vtil::Native::Callbacks::AIAudioAPI_Resume>(p[10])(); }
+Vtil::Native::AIAudioResultCode WINAPI Vtil::Native::AIAudioAPI_SaveWave(LPTSTR path, AIAudio_TWaveFormat& format, UINT8* buf, UINT32 len)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AIAudioAPI_SaveWave>(p[11])(path, format, buf, len); }
+Vtil::Native::AIAudioResultCode WINAPI Vtil::Native::AIAudioAPI_GetDescriptor(INT32 target, byte* desc, UINT32 bufferLen, UINT32& requireLen)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AIAudioAPI_GetDescriptor>(p[12])(target, desc, bufferLen, requireLen); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_Init(AITalk_TConfig& config)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_Init>(p[13])(config); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_End()
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_End>(p[14])(); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_VersionInfo(INT32 verbose, LPTSTR sjis, UINT32 bufferLen, UINT32& requireLen)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_VersionInfo>(p[15])(verbose, sjis, bufferLen, requireLen); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_GetStatus(INT32 jobID, AITalkStatusCode& status)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_GetStatus>(p[16])(jobID, status); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_SetParam(AITalk_TTtsParam& param)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_SetParam>(p[17])(param); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_GetParam(AITalk_TTtsParam& param, UINT32& size)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_GetParam>(p[18])(param, size); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_LangLoad(LPTSTR dirLang)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_LangLoad>(p[19])(dirLang); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_LangClear()
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_LangClear>(p[20])(); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_VoiceLoad(LPTSTR voiceName)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_VoiceLoad>(p[21])(voiceName); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_VoiceClear()
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_VoiceClear>(p[22])(); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_TextToSpeech(INT32& jobID, AITalk_TJobParam& param, LPTSTR text)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_TextToSpeech>(p[23])(jobID, param, text); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_CloseSpeech(INT32 jobID, INT32 useEvent)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_CloseSpeech>(p[24])(jobID, useEvent); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_GetData(INT32 jobID, INT16* rawBuf, UINT32 bufferLen, UINT32& requireLen)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_GetData>(p[25])(jobID, rawBuf, bufferLen, requireLen); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_TextToKana(INT32& jobID, AITalk_TJobParam& param, LPTSTR text)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_TextToKana>(p[26])(jobID, param, text); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_CloseKana(INT32 jobID, INT32 useEvent)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_CloseKana>(p[27])(jobID, useEvent); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_GetKana(INT32 jobID, LPTSTR textBuf, UINT32 bufferLen, UINT32& requireLen, UINT32& pos)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_GetKana>(p[28])(jobID, textBuf, bufferLen, requireLen, pos); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_GetJeitaControl(INT32 jobID, LPTSTR control)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_GetJeitaControl>(p[29])(jobID, control); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_BLoadWordDic()
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_BLoadWordDic>(p[30])(); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_ReloadWordDic(LPTSTR path)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_ReloadWordDic>(p[31])(path); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_ReloadPhraseDic(LPTSTR path)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_ReloadPhraseDic>(p[32])(path); }
+Vtil::Native::AITalkResultCode WINAPI Vtil::Native::AITalkAPI_ReloadSymbolDic(LPTSTR path)
+{ return reinterpret_cast<Vtil::Native::Callbacks::AITalkAPI_ReloadSymbolDic>(p[33])(path); }
 void WINAPI Vtil::Native::_1() { PA = p[34]; JMPtoAPI(); }
 void WINAPI Vtil::Native::_2() { PA = p[35]; JMPtoAPI(); }
 #pragma managed(pop)
--- Vtil/Vtil.h
+++ Vtil/Vtil.h
@@ -16,42 +16,219 @@ namespace Vtil
 	namespace Native
 	{
 #pragma managed(push, off)
+
+		enum class AITalkResultCode
+		{
+			AITALKERR_SUCCESS = 0,
+			AITALKERR_INTERNAL_ERROR = -1,
+			AITALKERR_UNSUPPORTED = -2,
+			AITALKERR_INVALID_ARGUMENT = -3,
+			AITALKERR_WAIT_TIMEOUT = -4,
+			AITALKERR_NOT_INITIALIZED = -10,
+			AITALKERR_ALREADY_INITIALIZED = 10,
+			AITALKERR_NOT_LOADED = -11,
+			AITALKERR_ALREADY_LOADED = 11,
+			AITALKERR_INSUFFICIENT = -20,
+			AITALKERR_PARTIALLY_REGISTERED = 21,
+			AITALKERR_LICENSE_ABSENT = -100,
+			AITALKERR_LICENSE_EXPIRED = -101,
+			AITALKERR_LICENSE_REJECTED = -102,
+			AITALKERR_TOO_MANY_JOBS = -201,
+			AITALKERR_INVALID_JOBID = -202,
+			AITALKERR_JOB_BUSY = -203,
+			AITALKERR_NOMORE_DATA = 204,
+			AITALKERR_OUT_OF_MEMORY = -206,
+			AITALKERR_FILE_NOT_FOUND = -1001,
+			AITALKERR_PATH_NOT_FOUND = -1002,
+			AITALKERR_READ_FAULT = -1003,
+			AITALKERR_COUNT_LIMIT = -1004,
+			AITALKERR_USERDIC_LOCKED = -1011,
+			AITALKERR_USERDIC_NOENTRY = -1012,
+		};
+
+		enum class AITalkStatusCode
+		{
+			AITALKSTAT_WRONG_STATE = -1,
+			AITALKSTAT_INPROGRESS = 10,
+			AITALKSTAT_STILL_RUNNING = 11,
+			AITALKSTAT_DONE = 12
+		};
+
+		enum class AITalkJobInOut
+		{
+			AITALKIOMODE_PLAIN_TO_WAVE = 11,
+			AITALKIOMODE_AIKANA_TO_WAVE = 12,
+			AITALKIOMODE_JEITA_TO_WAVE = 13,
+			AITALKIOMODE_PLAIN_TO_AIKANA = 21,
+			AITALKIOMODE_AIKANA_TO_JEITA = 0x20
+		};
+
+		enum class AIAudioResultCode
+		{
+			AIAUDIOERR_SUCCESS = 0,
+			AIAUDIOERR_INTERNAL_ERROR = -1,
+			AIAUDIOERR_UNSUPPORTED = -2,
+			AIAUDIOERR_INVALID_ARGUMENT = -3,
+			AIAUDIOERR_WAIT_TIMEOUT = -4,
+			AIAUDIOERR_NOT_INITIALIZED = -10,
+			AIAUDIOERR_NOT_OPENED = -11,
+			AIAUDIOERR_ALREADY_OPENED = 11,
+			AIAUDIOERR_NO_AUDIO_HARDWARE = -20,
+			AIAUDIOERR_DEVICE_INVALIDATED = -21,
+			AIAUDIOERR_INSUFFICIENT = -30,
+			AIAUDIOERR_OUT_OF_MEMORY = -100,
+			AIAUDIOERR_INVALID_CALL = -101,
+			AIAUDIOERR_INVALID_EVENT_ORDINAL = -102,
+			AIAUDIOERR_NO_NOTIFICATION_CALLBACK = 103,
+			AIAUDIOERR_NO_PLAYING = 111,
+			AIAUDIOERR_WRITE_FAULT = -201
+		};
+
+		enum class AIAudioFormatType
+		{
+			AIAUIDOTYPE_NONE = 0,
+			AIAUDIOTYPE_PCM_16 = 1,
+			AIAUDIOTYPE_PCM_8 = 769,
+			AIAUDIOTYPE_MULAW_8 = 7,
+			AIAUDIOTYPE_ALAW_8 = 6
+		};
+
+#pragma pack(1)
+		struct AITalk_TConfig
+		{
+			UINT32 hzVoiceDB;
+			LPTSTR dirVoiceDBS;
+			UINT32 msecTimeout;
+			LPTSTR pathLicense;
+			LPTSTR codeAuthSeed;
+			UINT32 __reserved__;
+		};
+
+		struct AITalk_TJobParam
+		{
+			AITalkJobInOut modeInOut;
+			void* userData;
+		};
+
+		struct AITalk_TTtsParam
+		{
+			UINT32 size;
+			void* procTextBuf;
+			void* procRawBuf;
+			void* procEventTts;
+			UINT32 lenTextBufBytes;
+			UINT32 lenRawBufBytes;
+			FLOAT volume;
+			INT32 pauseBegin;
+			INT32 pauseTerm;
+			UINT32 extendFormat;
+			TCHAR voiceName[80];
+			TCHAR femaleName[80];
+			TCHAR maleName[80];
+			INT32 pauseMiddle;
+			INT32 pauseLong;
+			INT32 pauseSentence;
+			TCHAR control[12];
+			UINT32 numSpeakers;
+			INT32 __reserved__;
+		};
+
+		struct AIAudio_TConfig
+		{
+			using AIAudioProcNotify = void(__stdcall*)(UINT64, void*);
+
+			AIAudioProcNotify procNotify;
+			UINT32 msecLatency;
+			UINT32 lenBufferBytes;
+			UINT32 hzSamplesPerSec;
+			AIAudioFormatType formatTag;
+			INT32 __reserved__;
+			LPTSTR descDevice;
+		};
+
+		struct AIAudio_TWaveFormat
+		{
+			INT32 header;
+			UINT32 hzSamplesPerSec;
+			AIAudioFormatType formatTag;
+		};
+#pragma pack()
+
+		namespace Callbacks
+		{
+			typedef AIAudioResultCode(WINAPI* AIAudioAPI_DeviceInfo)(LPTSTR guid, LPTSTR name, INT32 bufferLen, INT32& requireLen);
+			typedef UINT32(WINAPI* AITalkAPI_ModuleFlag)();
+			typedef AITalkResultCode(WINAPI* AITalkAPI_LicenseInfo)(LPTSTR key, LPTSTR str, INT32 len);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_LicenseDate)(LPTSTR data);
+			typedef AIAudioResultCode(WINAPI* AIAudioAPI_Open)(AIAudio_TConfig& config);
+			typedef AIAudioResultCode(WINAPI* AIAudioAPI_Close)();
+			typedef AIAudioResultCode(WINAPI* AIAudioAPI_PushData)(UINT8* buf, UINT32 len, INT32 stop);
+			typedef AIAudioResultCode(WINAPI* AIAudioAPI_PushEvent)(UINT64 tick, void* userData);
+			typedef AIAudioResultCode(WINAPI* AIAudioAPI_ClearData)();
+			typedef AIAudioResultCode(WINAPI* AIAudioAPI_Suspend)();
+			typedef AIAudioResultCode(WINAPI* AIAudioAPI_Resume)();
+			typedef AIAudioResultCode(WINAPI* AIAudioAPI_SaveWave)(LPTSTR path, AIAudio_TWaveFormat& format, UINT8* buf, UINT32 len);
+			typedef AIAudioResultCode(WINAPI* AIAudioAPI_GetDescriptor)(INT32 target, byte* desc, UINT32 bufferLen, UINT32& requireLen);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_Init)(AITalk_TConfig& config);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_End)();
+			typedef AITalkResultCode(WINAPI* AITalkAPI_VersionInfo)(INT32 verbose, LPTSTR sjis, UINT32 bufferLen, UINT32& requireLen);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_GetStatus)(INT32 jobID, AITalkStatusCode& status);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_SetParam)(AITalk_TTtsParam& param);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_GetParam)(AITalk_TTtsParam& param, UINT32& size);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_LangLoad)(LPTSTR dirLang);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_LangClear)();
+			typedef AITalkResultCode(WINAPI* AITalkAPI_VoiceLoad)(LPTSTR voiceName);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_VoiceClear)();
+			typedef AITalkResultCode(WINAPI* AITalkAPI_TextToSpeech)(INT32& jobID, AITalk_TJobParam& param, LPTSTR text);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_CloseSpeech)(INT32 jobID, INT32 useEvent);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_GetData)(INT32 jobID, INT16* rawBuf, UINT32 bufferLen, UINT32& requireLen);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_TextToKana)(INT32& jobID, AITalk_TJobParam& param, LPTSTR text);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_CloseKana)(INT32 jobID, INT32 useEvent);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_GetKana)(INT32 jobID, LPTSTR textBuf, UINT32 bufferLen, UINT32& requireLen, UINT32& pos);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_GetJeitaControl)(INT32 jobID, LPTSTR control);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_BLoadWordDic)();
+			typedef AITalkResultCode(WINAPI* AITalkAPI_ReloadWordDic)(LPTSTR path);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_ReloadPhraseDic)(LPTSTR path);
+			typedef AITalkResultCode(WINAPI* AITalkAPI_ReloadSymbolDic)(LPTSTR path);
+		}
+
 		extern "C"
 		{
-			void CLASS_DECLSPEC WINAPI AIAudioAPI_DeviceInfo();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_ModuleFlag();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_LicenseInfo();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_LicenseDate();
-			void CLASS_DECLSPEC WINAPI AIAudioAPI_Open();
-			void CLASS_DECLSPEC WINAPI AIAudioAPI_Close();
-			void CLASS_DECLSPEC WINAPI AIAudioAPI_PushData();
-			void CLASS_DECLSPEC WINAPI AIAudioAPI_PushEvent();
-			void CLASS_DECLSPEC WINAPI AIAudioAPI_ClearData();
-			void CLASS_DECLSPEC WINAPI AIAudioAPI_Suspend();
-			void CLASS_DECLSPEC WINAPI AIAudioAPI_Resume();
-			void CLASS_DECLSPEC WINAPI AIAudioAPI_SaveWave();
-			void CLASS_DECLSPEC WINAPI AIAudioAPI_GetDescriptor();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_Init();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_End();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_VersionInfo();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_GetStatus();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_SetParam();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_GetParam();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_LangLoad();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_LangClear();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_VoiceLoad();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_VoiceClear();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_TextToSpeech();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_CloseSpeech();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_GetData();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_TextToKana();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_CloseKana();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_GetKana();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_GetJeitaControl();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_BLoadWordDic();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_ReloadWordDic();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_ReloadPhraseDic();
-			void CLASS_DECLSPEC WINAPI AITalkAPI_ReloadSymbolDic();
+			AIAudioResultCode CLASS_DECLSPEC WINAPI AIAudioAPI_DeviceInfo(LPTSTR guid, LPTSTR name, INT32 bufferLen, INT32& requireLen);
+			UINT32 CLASS_DECLSPEC WINAPI AITalkAPI_ModuleFlag();
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_LicenseInfo(LPTSTR key, LPTSTR str, INT32 len);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_LicenseDate(LPTSTR data);
+			AIAudioResultCode CLASS_DECLSPEC WINAPI AIAudioAPI_Open(AIAudio_TConfig& config);
+			AIAudioResultCode CLASS_DECLSPEC WINAPI AIAudioAPI_Close();
+			AIAudioResultCode CLASS_DECLSPEC WINAPI AIAudioAPI_PushData(UINT8* buf, UINT32 len, INT32 stop);
+			AIAudioResultCode CLASS_DECLSPEC WINAPI AIAudioAPI_PushEvent(UINT64 tick, void* userData);
+			AIAudioResultCode CLASS_DECLSPEC WINAPI AIAudioAPI_ClearData();
+			AIAudioResultCode CLASS_DECLSPEC WINAPI AIAudioAPI_Suspend();
+			AIAudioResultCode CLASS_DECLSPEC WINAPI AIAudioAPI_Resume();
+			AIAudioResultCode CLASS_DECLSPEC WINAPI AIAudioAPI_SaveWave(LPTSTR path, AIAudio_TWaveFormat& format, UINT8* buf, UINT32 len);
+			AIAudioResultCode CLASS_DECLSPEC WINAPI AIAudioAPI_GetDescriptor(INT32 target, byte* desc, UINT32 bufferLen, UINT32& requireLen);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_Init(AITalk_TConfig& config);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_End();
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_VersionInfo(INT32 verbose, LPTSTR sjis, UINT32 bufferLen, UINT32& requireLen);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_GetStatus(INT32 jobID, AITalkStatusCode& status);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_SetParam(AITalk_TTtsParam& param);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_GetParam(AITalk_TTtsParam& param, UINT32& size);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_LangLoad(LPTSTR dirLang);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_LangClear();
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_VoiceLoad(LPTSTR voiceName);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_VoiceClear();
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_TextToSpeech(INT32& jobID, AITalk_TJobParam& param, LPTSTR text);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_CloseSpeech(INT32 jobID, INT32 useEvent);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_GetData(INT32 jobID, INT16* rawBuf, UINT32 bufferLen, UINT32& requireLen);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_TextToKana(INT32& jobID, AITalk_TJobParam& param, LPTSTR text);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_CloseKana(INT32 jobID, INT32 useEvent);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_GetKana(INT32 jobID, LPTSTR textBuf, UINT32 bufferLen, UINT32& requireLen, UINT32& pos);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_GetJeitaControl(INT32 jobID, LPTSTR control);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_BLoadWordDic();
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_ReloadWordDic(LPTSTR path);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_ReloadPhraseDic(LPTSTR path);
+			AITalkResultCode CLASS_DECLSPEC WINAPI AITalkAPI_ReloadSymbolDic(LPTSTR path);
+
 			#pragma comment(linker, "/export:??0_Mutex@std@@QEAA@W4_Uninitialized@1@@Z=_1")
 			void CLASS_DECLSPEC WINAPI _1();
 			#pragma comment(linker, "/export:??4_Init_locks@std@@QEAAAEAV01@AEBV01@@Z=_2")
