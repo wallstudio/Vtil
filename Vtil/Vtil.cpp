d64ca59585d7db8b33ab46e8596139994e4384fe
UpHash <yukawallstudio@gmail.com>
2021-05-16 01:16:33 +0900

    プロキシシンボルのエクスポート定義

--- Vtil/Vtil.cpp
+++ Vtil/Vtil.cpp
@@ -7,4 +7,41 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
 	return TRUE;
 }
+
+void WINAPI Vtil::Native::AIAudioAPI_DeviceInfo() {}
+void WINAPI Vtil::Native::AITalkAPI_ModuleFlag() {}
+void WINAPI Vtil::Native::AITalkAPI_LicenseInfo() {}
+void WINAPI Vtil::Native::AITalkAPI_LicenseDate() {}
+void WINAPI Vtil::Native::AIAudioAPI_Open() {}
+void WINAPI Vtil::Native::AIAudioAPI_Close() {}
+void WINAPI Vtil::Native::AIAudioAPI_PushData() {}
+void WINAPI Vtil::Native::AIAudioAPI_PushEvent() {}
+void WINAPI Vtil::Native::AIAudioAPI_ClearData() {}
+void WINAPI Vtil::Native::AIAudioAPI_Suspend() {}
+void WINAPI Vtil::Native::AIAudioAPI_Resume() {}
+void WINAPI Vtil::Native::AIAudioAPI_SaveWave() {}
+void WINAPI Vtil::Native::AIAudioAPI_GetDescriptor() {}
+void WINAPI Vtil::Native::AITalkAPI_Init() {}
+void WINAPI Vtil::Native::AITalkAPI_End() {}
+void WINAPI Vtil::Native::AITalkAPI_VersionInfo() {}
+void WINAPI Vtil::Native::AITalkAPI_GetStatus() {}
+void WINAPI Vtil::Native::AITalkAPI_SetParam() {}
+void WINAPI Vtil::Native::AITalkAPI_GetParam() {}
+void WINAPI Vtil::Native::AITalkAPI_LangLoad() {}
+void WINAPI Vtil::Native::AITalkAPI_LangClear() {}
+void WINAPI Vtil::Native::AITalkAPI_VoiceLoad() {}
+void WINAPI Vtil::Native::AITalkAPI_VoiceClear() {}
+void WINAPI Vtil::Native::AITalkAPI_TextToSpeech() {}
+void WINAPI Vtil::Native::AITalkAPI_CloseSpeech() {}
+void WINAPI Vtil::Native::AITalkAPI_GetData() {}
+void WINAPI Vtil::Native::AITalkAPI_TextToKana() {}
+void WINAPI Vtil::Native::AITalkAPI_CloseKana() {}
+void WINAPI Vtil::Native::AITalkAPI_GetKana() {}
+void WINAPI Vtil::Native::AITalkAPI_GetJeitaControl() {}
+void WINAPI Vtil::Native::AITalkAPI_BLoadWordDic() {}
+void WINAPI Vtil::Native::AITalkAPI_ReloadWordDic() {}
+void WINAPI Vtil::Native::AITalkAPI_ReloadPhraseDic() {}
+void WINAPI Vtil::Native::AITalkAPI_ReloadSymbolDic() {}
+void WINAPI Vtil::Native::_1() {}
+void WINAPI Vtil::Native::_2() {}
 #pragma managed(pop)
--- Vtil/Vtil.h
+++ Vtil/Vtil.h
@@ -7,4 +7,56 @@ namespace Vtil
 	{
 		// TODO: このクラスのメソッドをここに追加します。
 	};
+
+	#ifdef _EXPORTING
+	#define CLASS_DECLSPEC __declspec(dllexport)
+	#else
+	#define CLASS_DECLSPEC __declspec(dllimport)
+	#endif
+	namespace Native
+	{
+#pragma managed(push, off)
+		extern "C"
+		{
+			void CLASS_DECLSPEC WINAPI AIAudioAPI_DeviceInfo();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_ModuleFlag();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_LicenseInfo();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_LicenseDate();
+			void CLASS_DECLSPEC WINAPI AIAudioAPI_Open();
+			void CLASS_DECLSPEC WINAPI AIAudioAPI_Close();
+			void CLASS_DECLSPEC WINAPI AIAudioAPI_PushData();
+			void CLASS_DECLSPEC WINAPI AIAudioAPI_PushEvent();
+			void CLASS_DECLSPEC WINAPI AIAudioAPI_ClearData();
+			void CLASS_DECLSPEC WINAPI AIAudioAPI_Suspend();
+			void CLASS_DECLSPEC WINAPI AIAudioAPI_Resume();
+			void CLASS_DECLSPEC WINAPI AIAudioAPI_SaveWave();
+			void CLASS_DECLSPEC WINAPI AIAudioAPI_GetDescriptor();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_Init();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_End();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_VersionInfo();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_GetStatus();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_SetParam();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_GetParam();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_LangLoad();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_LangClear();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_VoiceLoad();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_VoiceClear();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_TextToSpeech();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_CloseSpeech();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_GetData();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_TextToKana();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_CloseKana();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_GetKana();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_GetJeitaControl();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_BLoadWordDic();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_ReloadWordDic();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_ReloadPhraseDic();
+			void CLASS_DECLSPEC WINAPI AITalkAPI_ReloadSymbolDic();
+			#pragma comment(linker, "/export:??0_Mutex@std@@QEAA@W4_Uninitialized@1@@Z=_1")
+			void CLASS_DECLSPEC WINAPI _1();
+			#pragma comment(linker, "/export:??4_Init_locks@std@@QEAAAEAV01@AEBV01@@Z=_2")
+			void CLASS_DECLSPEC WINAPI _2();
+		}
+#pragma managed(pop)
+	}
 }
