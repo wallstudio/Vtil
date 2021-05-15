cad3cf34b19d768c78b1e5fa2cc96f184be0ad57
UpHash <yukawallstudio@gmail.com>
2021-05-16 00:41:19 +0900

    リンクされることができるようにまで

--- Vtil/Vtil.cpp
+++ Vtil/Vtil.cpp
@@ -1,2 +1,10 @@
+#include <windows.h>
 #include "Vtil.h"
 
+
+#pragma managed(push, off)
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	return TRUE;
+}
+#pragma managed(pop)
--- Vtil/Vtil.h
+++ Vtil/Vtil.h
@@ -1,8 +1,8 @@
 ﻿#pragma once
 
-using namespace System;
 
-namespace Vtil {
+namespace Vtil
+{
 	public ref class Class1
 	{
 		// TODO: このクラスのメソッドをここに追加します。
